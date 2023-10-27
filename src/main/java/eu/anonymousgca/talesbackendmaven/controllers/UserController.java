8472638053c958155c8f8ead894ec92d42f348a7
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-10-28 00:12:22 +0200

    Changelogs: - Playing with POST endpoints

--- src/main/java/eu/anonymousgca/talesbackendmaven/controllers/UserController.java
+++ src/main/java/eu/anonymousgca/talesbackendmaven/controllers/UserController.java
@@ -1,7 +1,9 @@
 package eu.anonymousgca.talesbackendmaven.controllers;
 
+import eu.anonymousgca.talesbackendmaven.entities.Content;
 import eu.anonymousgca.talesbackendmaven.entities.User;
 import eu.anonymousgca.talesbackendmaven.exceptions.UserNotFoundException;
+import eu.anonymousgca.talesbackendmaven.interfaces.ContentRepository;
 import eu.anonymousgca.talesbackendmaven.interfaces.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ContentRepository contentRepository;
 
     @GetMapping
     public List<User> getAllUsers() {
@@ -53,7 +58,165 @@ public class UserController {
         return user;
     }
 
-    @GetMapping("/{username}/activation")
+    // Get user content
+    @GetMapping("/{username}/content")
+    public List<Content> getUserContent(@PathVariable String username) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // check if present, otherwise throw exception
+        return contentRepository.findByOwnerId(username);
+    }
+
+    @PostMapping("/{username}/urlProfilePicture")
+    public User setUserUrlProfilePicturePost(@PathVariable String username, @RequestBody String urlProfilePicture) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // Check if urlProfilePicture is null, if it's, throw error
+        if (urlProfilePicture == null) {
+            throw new IllegalArgumentException("urlProfilePicture cannot be null");
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setUrlProfilePicture(urlProfilePicture);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @PostMapping("/{username}/urlCoverPicture")
+    public User setUserUrlCoverPicturePost(@PathVariable String username, @RequestBody String urlCoverPicture) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // Check if urlCoverPicture is null, if it's, throw error
+        if (urlCoverPicture == null) {
+            throw new IllegalArgumentException("urlCoverPicture cannot be null");
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setUrlCoverPicture(urlCoverPicture);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @PostMapping("/{username}/description")
+    public User setUserDescriptionPost(@PathVariable String username, @RequestBody String description) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // Check if description is null, if it's, put an empty string
+        if (description == null) {
+            description = "";
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setDescription(description);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @PostMapping("/{username}/motto")
+    public User setUserMottoPost(@PathVariable String username, @RequestBody String motto) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // Check if motto is null, if it's, put an empty string
+        if (motto == null) {
+            motto = "";
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setMotto(motto);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @PostMapping("/{username}/showNSFW")
+    public User setUserShowNSFW(@PathVariable String username, @RequestBody boolean showNSFW) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setShowNSFW(showNSFW);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @PostMapping("/{username}/ofAge")
+    public User setUserOfAge(@PathVariable String username, @RequestBody boolean ofAge) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setOfAge(ofAge);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @PostMapping("/{username}/isMuted")
+    public User setUserIsMuted(@PathVariable String username, @RequestBody boolean isMuted) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setIsMuted(isMuted);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @PostMapping("/{username}/canUpload")
+    public User setUserCanUploadPost(@PathVariable String username, @RequestBody boolean canUpload) {
+
+        if (username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        // check if present, otherwise throw exception
+        User user = userRepository.findById(username).orElseThrow(() -> new UserNotFoundException(username));
+
+        user.setCanUpload(canUpload);
+        userRepository.save(user);
+
+        return user;
+    }
 
     @PostMapping
     public void addUser(@RequestBody User user) {
