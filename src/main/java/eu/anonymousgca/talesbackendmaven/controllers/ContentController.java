c7d818c1dc50a78c55fc5b6b737dfa1e9a2edf25
AnonymousGCA <39743848+GABRYCA@users.noreply.github.com>
2023-11-09 18:13:13 +0100

    Changelogs: - API Versioning.
    WARNING:
    - May start from Scratch.

--- src/main/java/eu/anonymousgca/talesbackendmaven/controllers/ContentController.java
+++ src/main/java/eu/anonymousgca/talesbackendmaven/controllers/ContentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/content")
+@RequestMapping("/api/v1/content")
 public class ContentController {
 
     ///////////////////////////////////
--- src/main/java/eu/anonymousgca/talesbackendmaven/controllers/GalleryController.java
+++ src/main/java/eu/anonymousgca/talesbackendmaven/controllers/GalleryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/gallery")
+@RequestMapping("/api/v1/gallery")
 public class GalleryController {
 
     @Autowired
